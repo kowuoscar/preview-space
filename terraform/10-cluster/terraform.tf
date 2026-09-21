db33e849067eb85ed33c06427a66295bb2320c69
kowuoscar <kowuoscar@gmail.com>
2026-09-22 00:19:22 +0200

    fix(lint): run terraform validate and fmt

--- terraform/10-cluster/terraform.tf
+++ terraform/10-cluster/terraform.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 
   backend "s3" {
-    bucket = "preview-space-452183714072"
-    region = "eu-west-1"
-    key = "terraform/10-cluster"
+    bucket         = "preview-space-452183714072"
+    region         = "eu-west-1"
+    key            = "terraform/10-cluster"
     dynamodb_table = "PreviewSpaceStateLockTable-452183714072"
-    encrypt = true
+    encrypt        = true
   }
 }
--- terraform/10-cluster/variables.tf
+++ terraform/10-cluster/variables.tf
@@ -1,24 +1,24 @@
 variable "region" {
-  type = string
+  type    = string
   default = "eu-west-1"
   validation {
-    condition = can(regex("^eu-", var.region))
+    condition     = can(regex("^eu-", var.region))
     error_message = "EU regions only."
   }
 }
 
 variable "project" {
-  type = string
+  type    = string
   default = "preview-space"
 }
 
 variable "env" {
-  type = string
+  type    = string
   default = "Dev"
 }
 
 variable "node_groups" {
-  type = map(object({instance_type = string, desired = number }))
+  type = map(object({ instance_type = string, desired = number }))
   default = {
     "general" = { instance_type = "t3.medium", desired = 2 }
   }
@@ -26,10 +26,5 @@ variable "node_groups" {
 
 locals {
   cluster_name = "${var.project}-${var.env}"
-  tags = { Project = var.project, ManagedBy = "terraform" }
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-  sensitive = false
+  tags         = { Project = var.project, ManagedBy = "terraform" }
 }
